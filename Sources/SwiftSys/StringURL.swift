6742ff88a088713484e7941caaa1c21d2b0e58cd
Philip Chu <technicat@users.noreply.github.com>
2022-07-12 12:22:45 -0700

    add openweb

---
+++ Sources/SwiftSys/StringURL.swift
@@ -0,0 +1,21 @@
+//  Created by Philip Chu on 7/29/15.
+//  Copyright (c) 2015 Technicat. All rights reserved.
+//
+
+import UIKit
+
+extension String {
+    func openWeb() {
+        if let url = URL(string: self) {
+            UIApplication.shared.open(url)
+        } else {
+            guard let new = addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+                Sys.log.error("Unable to urlencode \(self)")
+                return
+            }
+            if let url = URL(string: new) {
+                UIApplication.shared.open(url)
+            }
+        }
+    }
+}
--- Sources/SwiftSys/SysSettings.swift
+++ Sources/SwiftSys/SysSettings.swift
@@ -1,7 +1,3 @@
-//
-//  Sys.swift
-//  Fugu Dim Sum
-//
 //  Created by Philip Chu on 10/25/20.
 //  Copyright © 2020 Technicat. All rights reserved.
 //
