c0109a5daa246ed6839814229ce800616a1ff29a
Philip Chu <technicat@users.noreply.github.com>
2022-07-16 15:52:58 -0700

    Update FileManager.swift

--- Sources/SwiftSys/FileManager.swift
+++ Sources/SwiftSys/FileManager.swift
@@ -4,15 +4,14 @@
 
 import Foundation
 
-// todo - move to swiftsys
 extension FileManager {
-    var documentsDirectory: URL {
+    public var documentsDirectory: URL {
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         let documentsDirectory = paths[0]
         return documentsDirectory
     }
 
-    func documentsPath(_ name: String) -> URL {
+    public func documentsPath(_ name: String) -> URL {
         FileManager.default.documentsDirectory.appendingPathComponent(name)
     }
 }
