56fd0e77f732a6aae9cb96e7b9280c2d1867b93c
Philip Chu <technicat@users.noreply.github.com>
2022-09-20 10:01:28 -0700

    Create URLButton.swift

---
+++ Sources/SwiftSys/UI/URLButton.swift
@@ -0,0 +1,18 @@
+//  Created by Philip Chu on 10/22/19.
+//  Copyright © 2019 Technicat. All rights reserved.
+//
+
+import SwiftSys
+import SwiftUI
+
+struct URLButton: View {
+    let url: String
+    let name: String
+
+    var body: some View {
+        Button(action: { url.openWeb() },
+               label: {
+                   Label(name, systemImage: "link")
+               })
+    }
+}
