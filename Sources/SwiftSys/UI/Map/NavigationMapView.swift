e1f206a67e56f18cd501873e5d95bfd6a3753831
Philip Chu <technicat@users.noreply.github.com>
2022-09-22 09:58:23 -0700

    map

---
+++ Sources/SwiftSys/UI/Map/NavigationMapView.swift
@@ -0,0 +1,34 @@
+//  Created by Philip Chu on 9/26/19.
+//  Copyright © 2019 Technicat. All rights reserved.
+
+import MapKit
+import SwiftUI
+
+@available(macOS 11.0, *)
+struct NavigationMapView: View {
+    let region: Region
+
+    @State private var cregion = MKCoordinateRegion()
+
+    init(region: Region, place: Place) {
+        self.region = region
+        _cregion = .init(initialValue:
+            MKCoordinateRegion(center: place.coordinate,
+                               span: MKCoordinateSpan(latitudeDelta: 0.005,
+                                                      longitudeDelta: 0.005)))
+    }
+
+    var body: some View {
+        Map(coordinateRegion: $cregion,
+            showsUserLocation: true,
+            annotationItems: region.places,
+            annotationContent: { place in
+                MapAnnotation(
+                    coordinate: place.coordinate,
+                    anchorPoint: CGPoint(x: 0.5, y: 0.5)
+                ) {
+                    MapMarker(place: place)
+                }
+            })
+    }
+}
