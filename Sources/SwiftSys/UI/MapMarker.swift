0dbdc2d0cec1c3846f3b58670f820047df27ab4a
Philip Chu <technicat@users.noreply.github.com>
2022-09-22 09:53:48 -0700

    Create MapMarker.swift

---
+++ Sources/SwiftSys/UI/MapMarker.swift
@@ -0,0 +1,20 @@
+//  Created by Philip Chu on 9/26/19.
+//  Copyright © 2019 Technicat. All rights reserved.
+
+import SwiftUI
+
+@available(macOS 11.0, *)
+struct MapMarker: View {
+    let place: Place
+
+    var body: some View {
+        VStack {
+            Image(systemName: "mappin")
+                .imageScale(.large)
+                .foregroundColor(.red)
+            Text(place.name)
+                .font(.callout)
+                .foregroundColor(.red)
+        }
+    }
+}
