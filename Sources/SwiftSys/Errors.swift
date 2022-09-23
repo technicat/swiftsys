0a622fefad46f1905b6765244f56d06211025bd7
Philip Chu <technicat@users.noreply.github.com>
2022-09-23 15:15:44 -0700

    Update Errors.swift

--- Sources/SwiftSys/Errors.swift
+++ Sources/SwiftSys/Errors.swift
@@ -1,6 +1,8 @@
 //  Created by Philip Chu on 1/21/19.
 //  Copyright © 2019 Technicat. All rights reserved.
 
+// cases should be lowercase?
+
 public enum FileError : Error {
     case NotFound(_ file: String)
 }
