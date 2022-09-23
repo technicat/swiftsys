be67edffb323554a16c84b67028dd4b65d9ba15f
Philip Chu <technicat@users.noreply.github.com>
2022-09-22 17:05:44 -0700

    Create Chinese.swift

---
+++ Sources/SwiftSys/Language/Chinese.swift
@@ -0,0 +1,34 @@
+//  Created by Philip Chu on 7/31/15.
+//  Copyright (c) 2015 Technicat. All rights reserved.
+
+public enum Chinese {
+    case cantonese
+    case mandarin
+    case simplified
+    
+    // language codes used for TTS and STT
+    public var spoken: String {
+        switch self {
+        case .cantonese: return "zh-HK"
+        case .mandarin: return "zh-TW"
+        case .simplified: return "zh-CN"
+        }
+    }
+    
+    // could also use this as a localization key
+    public var label: String {
+        switch self {
+        case .cantonese: return "Cantonese"
+        case .mandarin: return "Mandarin"
+        case .simplified: return "Simplified"
+        }
+    }
+}
+
+extension Chinese : CaseIterable {
+    public static func language(_ index: Int) -> Chinese {
+        Chinese.allCases[index]
+        // should check for out of range
+    }
+}
+
