f3e8a3117044c87e98bf88b385f6c99f584832de
Philip Chu <technicat@users.noreply.github.com>
2022-09-25 13:35:10 -0700

    Update StringWeb.swift

--- Sources/SwiftSys/Extensions/String/StringWeb.swift
+++ Sources/SwiftSys/Extensions/String/StringWeb.swift
@@ -3,14 +3,18 @@
 //
 #if !os(macOS)
     import UIKit
+//#else
+ //   import Foundation
+//#endif
 
     public extension String {
 
         func openWeb() throws {
+            // should check canOpenURL
             if let url = URL(string: self) {
                 UIApplication.shared.open(url)
             } else {
-                if let url = URL(string: self.urlEncode()) {
+                if let url = try? URL(string: self.urlEncode()) {
                     UIApplication.shared.open(url)
                 } else {
                     throw StringError.urlFail(self)
