14be7e3896c473232fd6072d0a32af64df5ce201
Philip Chu <technicat@users.noreply.github.com>
2022-09-25 17:09:22 -0700

    Line

---
+++ Sources/SwiftSys/Language/Line/Line.swift
@@ -0,0 +1,14 @@
+//  Created by Philip Chu on 12/19/18.
+//  Copyright © 2018 Technicat. All rights reserved.
+
+public struct Line: Identifiable {
+    public var id: Int = 0
+    public var name: String
+    public var words: [Word]
+
+    public init(id: Int, name: String, words: [Word]) {
+        self.id = id
+        self.name = name
+        self.words = words
+    }
+}
---
+++ Sources/SwiftSys/Language/Line/LineHashable.swift
@@ -0,0 +1,12 @@
+//  Created by Philip Chu on 12/19/18.
+//  Copyright © 2018 Technicat. All rights reserved.
+
+extension Line: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
+
+public func == (lhs: Line, rhs: Line) -> Bool {
+    lhs.id == rhs.id
+}
