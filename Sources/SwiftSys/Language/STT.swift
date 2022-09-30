dc78c25a02be3bc5632d60ab39bf73802f0f5466
Philip Chu <technicat@users.noreply.github.com>
2022-09-29 20:45:46 -0700

    STT

---
+++ Sources/SwiftSys/Language/STT.swift
@@ -0,0 +1,12 @@
+//  Created by Philip Chu on 4/24/15.
+//  Copyright (c) 2015 Technicat. All rights reserved.
+
+
+public enum STT {
+    case notlistening
+    case listening
+    case fail
+    case success
+    case error
+}
+
