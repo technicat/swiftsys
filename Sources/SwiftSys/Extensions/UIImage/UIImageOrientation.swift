353a8f1dfca0710b039ab5992ff8b6f5a8df8be9
Philip Chu <technicat@users.noreply.github.com>
2022-09-30 19:56:13 -0700

    uiimage orientation

---
+++ Sources/SwiftSys/Extensions/UIImage/UIImageOrientation.swift
@@ -0,0 +1,24 @@
+//  Created by Philip Chu on 11/3/19.
+//  Copyright © 2019 Technicat. All rights reserved.
+
+#if os(iOS)
+import UIKit
+
+extension UIImage.Orientation {
+    init(_ cgOrientation: UIImage.Orientation) {
+        switch cgOrientation {
+        case .up: self = .up
+        case .upMirrored: self = .upMirrored
+        case .down: self = .down
+        case .downMirrored: self = .downMirrored
+        case .left: self = .left
+        case .leftMirrored: self = .leftMirrored
+        case .right: self = .right
+        case .rightMirrored: self = .rightMirrored
+        @unknown default:
+            Sys.log.warning("unknown orientation")
+            self = .up
+        }
+    }
+}
+#endif
