7f8a7240cba4bb05198bdf49077cb92cfe2a3068
Philip Chu <technicat@users.noreply.github.com>
2022-09-30 20:21:44 -0700

    Update UIImageOrientation.swift

--- Sources/SwiftSys/Extensions/UIImage/UIImageOrientation.swift
+++ Sources/SwiftSys/Extensions/UIImage/UIImageOrientation.swift
@@ -4,6 +4,24 @@
 #if os(iOS)
 import UIKit
 
+extension CGImagePropertyOrientation {
+    init(_ uiOrientation: UIImage.Orientation) {
+        switch uiOrientation {
+        case .up: self = .up
+        case .upMirrored: self = .upMirrored
+        case .down: self = .down
+        case .downMirrored: self = .downMirrored
+        case .left: self = .left
+        case .leftMirrored: self = .leftMirrored
+        case .right: self = .right
+        case .rightMirrored: self = .rightMirrored
+        @unknown default:
+            Sys.log.warning("unknown orientation")
+            self = .up
+        }
+    }
+}
+
 extension UIImage.Orientation {
     init(_ cgOrientation: UIImage.Orientation) {
         switch cgOrientation {
