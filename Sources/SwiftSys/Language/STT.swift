d6291b46dbef3ee28ab431accd86168f1eb8a049
Philip Chu <technicat@users.noreply.github.com>
2022-10-14 20:05:44 -0700

    add STTError

--- Sources/SwiftSys/Language/STT.swift
+++ Sources/SwiftSys/Language/STT.swift
@@ -1,12 +1,18 @@
 //  Created by Philip Chu on 4/24/15.
 //  Copyright (c) 2015 Technicat. All rights reserved.
 
-
 public enum STT {
     case notlistening
     case listening
     case fail
     case success
     case error
+}
+
+enum STTError: Error {
+    case startEngine
+    case noRecognizer
+    case noSampleRate
+    case unavailableRecognizer
 }
 
