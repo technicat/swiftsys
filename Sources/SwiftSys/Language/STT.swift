9a18aa3f818cfb747872efca9bd276d93125ce53
Philip Chu <technicat@users.noreply.github.com>
2022-10-16 18:16:08 -0700

    add tapNode and untapNode to STT

--- Sources/SwiftSys/Language/STT.swift
+++ Sources/SwiftSys/Language/STT.swift
@@ -3,12 +3,31 @@
 
 import Speech
 
+@available(macOS 10.15, *)
 public enum STT {
     case notlistening
     case listening
     case fail
     case success
     case error
+    
+    public func tapNode(_ node: AVAudioInputNode,
+                           _ request: SFSpeechAudioBufferRecognitionRequest) throws {
+        let recordingFormat = node.outputFormat(forBus: 0)
+        // also check channel count? per input node doc
+        guard recordingFormat.sampleRate > 0 else {
+            throw STTError.noSampleRate
+        }
+        node.installTap(onBus: 0,
+            bufferSize: 1024,
+            format: recordingFormat) { buffer, _ in
+            request.append(buffer)
+        }
+    }
+    static public func untapNode(_ node: AVAudioInputNode) {
+        node.removeTap(onBus: 0)
+    }
+
 }
 
 public enum STTError: Error {
