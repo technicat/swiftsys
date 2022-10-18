e7574d228decc87be7c23c3084c80616833e0f73
Philip Chu <technicat@users.noreply.github.com>
2022-10-17 18:19:52 -0700

    fix for speech to text munging text to speech volume

--- Sources/SwiftSys/Language/TTS.swift
+++ Sources/SwiftSys/Language/TTS.swift
@@ -2,6 +2,7 @@
 //  Copyright (c) 2015 Technicat. All rights reserved.
 
 import AVFoundation
+import AVFAudio
 
 @available(macOS 11, *)
 public class TTS {
@@ -24,9 +25,18 @@ public class TTS {
         rate: Float = 0.3,
         volume: Float = 1.0) throws
     {
+        //https://stackoverflow.com/questions/53619027/avspeechsynthesizer-volume-too-low
+        #if os(iOS)
+            let audioSession = AVAudioSession.sharedInstance()
+            try audioSession.setCategory(AVAudioSession.Category.playAndRecord)
+            try audioSession.setMode(AVAudioSession.Mode.default)
+            try audioSession.setActive(true)
+            try AVAudioSession.sharedInstance().overrideOutputAudioPort(AVAudioSession.PortOverride.speaker)
+        #endif
         if synth == nil {
             synth = AVSpeechSynthesizer()
         }
+
         let utterance = AVSpeechUtterance(string: words)
         // should we precreate voices?
         guard let voice = AVSpeechSynthesisVoice(language: language.spoken) else {
