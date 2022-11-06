8b5b0f4596f1d7a27973f8233b7795aa662e3d1a
Philip Chu <technicat@users.noreply.github.com>
2022-11-06 13:38:05 -0800

    urlCreate

--- Sources/SwiftSys/Extensions/String/StringURL.swift
+++ Sources/SwiftSys/Extensions/String/StringURL.swift
@@ -1,6 +1,8 @@
 //  Created by Philip Chu on 7/29/15.
 //  Copyright (c) 2015 Technicat. All rights reserved.
 
+import Foundation
+
 public extension String {
     
     func urlEncode() throws -> String  {
@@ -8,5 +10,17 @@ public extension String {
             throw StringError.urlEncodeFail(self)
         }
         return new
+    }
+    
+    func urlCreate() throws -> URL {
+        if let url = URL(string: self) {
+            return url
+        } else {
+            if let url = try? URL(string: self.urlEncode()) {
+                return url
+            } else {
+                throw StringError.urlEncodeFail(self)
+            }
+        }
     }
 }
--- Sources/SwiftSys/Extensions/String/StringWeb.swift
+++ Sources/SwiftSys/Extensions/String/StringWeb.swift
@@ -1,19 +1,9 @@
 //  Created by Philip Chu on 7/29/15.
 //  Copyright (c) 2015 Technicat. All rights reserved.
 
-import SwiftUI
-
 public extension String {
 
     func openWeb() throws {
-        if let url = URL(string: self) {
-            try url.open()
-        } else {
-            if let url = try? URL(string: self.urlEncode()) {
-                try url.open()
-            } else {
-                throw StringError.urlEncodeFail(self)
-            }
-        }
+        try self.urlCreate().open()
     }
 }
