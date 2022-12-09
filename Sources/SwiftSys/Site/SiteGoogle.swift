dce7ffe5c4edbfc744cf4625fdd9f20badf846e7
Philip Chu <technicat@users.noreply.github.com>
2022-12-09 11:17:36 -0800

    Update SiteGoogle.swift

--- Sources/SwiftSys/Site/SiteGoogle.swift
+++ Sources/SwiftSys/Site/SiteGoogle.swift
@@ -2,16 +2,12 @@
 //  Copyright © 2016 Technicat. All rights reserved.
 
 import Foundation
+import MapKit
 
 extension Site {
-
-    public init? (lat: Double, lon: Double) {
-    let url = "https://www.google.com/maps/@?api=1&map_action=map&center=\(lat)%2C\(lon)"
-        self.init("google maps", url: url)
-    }
     
-    public init? (gmap: (lat: Double, lon: Double)) {
-        let url = "https://www.google.com/maps/@?api=1&map_action=map&center=\(gmap.lat)%2C\(gmap.lon)"
+    public init? (gmap: CLLocationCoordinate2D) {
+        let url = "https://www.google.com/maps/@?api=1&map_action=map&center=\(gmap.latitude)%2C\(gmap.longitude)"
         self.init("google maps", url: url)
     }
 }
