5932bce6b66a870336e3262f1123f68f91bce13d
Philip Chu <technicat@users.noreply.github.com>
2022-12-18 11:47:15 -0800

    Update ItemMarkdown.swift

--- Sources/SwiftSys/Language/Item/ItemMarkdown.swift
+++ Sources/SwiftSys/Language/Item/ItemMarkdown.swift
@@ -4,23 +4,33 @@
 import Foundation
 import RegexBuilder
 
+@available(macOS 13, *)
+@available(iOS 16, *)
 extension Item {
     
-    @available(macOS 12, *)
-    @available(iOS 15, *)
     public var markdown: AttributedString {
-        (try? AttributedString(markdown: description,
+        (try? AttributedString(markdown: Item.processDesc(description),
                                options: AttributedString.MarkdownParsingOptions(interpretedSyntax: 
                                     .inlineOnlyPreservingWhitespace)))
                                     ?? AttributedString()
     }
     
-    @available(macOS 13, *)
-    @available(iOS 16, *)
     private static let regex = Regex {
         "{"
         Capture(OneOrMore(.word))
         "}"
 
+    }
+    
+    private static func processDesc(_ desc: String) -> String {
+        let matches = Set(desc.matches(of: regex).map{ $0.output.1 })
+        var res = desc
+        for match in matches {
+            if let site = Site(wkty: String(match)) {
+                res = res.replacingOccurrences(of: "{\(match)}",
+                                               with: site.markdown)
+            }
+        }
+        return res
     }
 }
