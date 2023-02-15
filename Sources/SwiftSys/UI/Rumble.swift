592e1062d6b7e7d6d8709ecc39b441cb2d80feba
Philip Chu <technicat@users.noreply.github.com>
2023-02-14 23:33:14 -0800

    Update Rumble.swift

--- Sources/SwiftSys/UI/Rumble.swift
+++ Sources/SwiftSys/UI/Rumble.swift
@@ -6,10 +6,22 @@
 
     public class Rumble {
         public static var light = UIImpactFeedbackGenerator(style: .light)
+        
+        public static func light() {
+            light.impactOccurred()
+        }
 
         public static var soft = UIImpactFeedbackGenerator(style: .soft)
+        
+        public static func soft() {
+            soft.impactOccurred()
+        }
 
         public static var medium = UIImpactFeedbackGenerator(style: .medium)
+        
+        public static func medium() {
+            medium.impactOccurred()
+        }
     }
 #endif
 
