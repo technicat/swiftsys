ce6061aae7e7ff076a3fbb1b5e8925db157e7b6d
Philip Chu <technicat@users.noreply.github.com>
2023-02-19 16:59:26 -0800

    Create StringCopy.swift

---
+++ Sources/SwiftSys/Extensions/String/StringCopy.swift
@@ -0,0 +1,16 @@
+//  Created by Philip Chu on 7/29/15.
+//  Copyright (c) 2015 Technicat. All rights reserved.
+
+#if os(iOS)
+    import MobileCoreServices
+    import UniformTypeIdentifiers
+
+    public extension String {
+
+        func copy() {
+            UIPasteboard.general.setValue(self,
+                forPasteboardType:
+                    UTType.plainText.identifier)
+        }
+    }
+#endif
