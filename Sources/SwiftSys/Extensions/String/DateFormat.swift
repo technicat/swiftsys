708b860971f934fe26f7ff90296cb2ec8d220b2c
Philip Chu <technicat@users.noreply.github.com>
2023-02-25 13:14:09 -0800

    Create DateFormat.swift

---
+++ Sources/SwiftSys/Extensions/String/DateFormat.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  
+//
+//  Created by Philip Chu on 2/25/23.
+//
+
+import Foundation
+
+@available(macOS 12.0, *)
+public extension Date {
+
+    var shortDate: String {
+        self.formatted(date: .abbreviated, time: .omitted)
+    }
+
+    var longDate: String {
+        self.formatted(date: .long, time: .omitted)
+    }
+
+    var dayDate: String {
+        self.formatted(date: .complete, time: .omitted)
+    }
+
+    var numDate: String {
+        self.formatted(date: .numeric, time: .omitted)
+    }
+}
