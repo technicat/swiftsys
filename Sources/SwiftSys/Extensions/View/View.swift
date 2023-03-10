17f2a95dc248ed5b94814c6284f4b249f3d27b24
Philip Chu <technicat@users.noreply.github.com>
2023-03-10 14:44:19 -0800

    Update View.swift

--- Sources/SwiftSys/Extensions/View/View.swift
+++ Sources/SwiftSys/Extensions/View/View.swift
@@ -7,6 +7,10 @@ import SwiftUI
 @available(macOS 10.15, *)
 public extension View {
 
+    /// make the View round, equivalent to applying a corner radius of half the width/height
+    /// (assumes the View is square, otherwise will result in a capsule
+    /// - Parameters
+    ///     - radius: radius of the circle, should be half the width/height of the View
     func round(_ radius: CGFloat) -> some View {
         self.frame(width: radius*2, height: radius*2)
         .cornerRadius(radius)
