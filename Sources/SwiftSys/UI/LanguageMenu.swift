e1f2f6993c345ac26c30a9c4e8c5fbeea07dd682
Philip Chu <technicat@users.noreply.github.com>
2023-04-04 12:19:42 -0700

    Create LanguageMenu.swift

---
+++ Sources/SwiftSys/UI/LanguageMenu.swift
@@ -0,0 +1,25 @@
+//
+//  Created by Philip Chu on 4/4/23.
+//
+
+import SwiftUI
+
+@available(macOS 11.0, *)
+public struct LanguageMenu: View {
+
+    @Binding var language: ISO639_1
+    
+    public var body: some View {
+        Menu {
+            ForEach(ISO639_1.allCases.sorted(by: { $0.display > $1.display})) { lang in
+                ActionButton(lang.display,
+                    sysImage: "character.book.closed") {
+                    language = lang
+                }
+            }
+        } label: {
+            Label(LocalizedStringKey(language.display),
+                  systemImage: "character.book.closed")
+        }
+    }
+}
