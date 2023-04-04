b6b7655e51a90243e21e591b775ab0e2d28bdca1
Philip Chu <technicat@users.noreply.github.com>
2023-04-04 13:12:24 -0700

    Update ChineseIndex.swift

--- Sources/SwiftSys/Language/Chinese/ChineseIndex.swift
+++ Sources/SwiftSys/Language/Chinese/ChineseIndex.swift
@@ -1,6 +1,7 @@
 //  Created by Philip Chu on 7/31/15.
 //  Copyright (c) 2015 Technicat. All rights reserved.
 
+/// no need to use these anymore since AppStorage can save enum
 extension Chinese {
 
     public static func language(_ index: Int) throws -> Chinese {
