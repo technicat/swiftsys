018579e9e530e312ec491e98055c39bd84c0f392
Philip Chu <technicat@users.noreply.github.com>
2023-04-05 01:22:28 -0700

    fixed missing language codes

--- Sources/SwiftSys/Language/ISO639/ISO639a2.swift
+++ Sources/SwiftSys/Language/ISO639/ISO639a2.swift
@@ -6,7 +6,7 @@ public enum ISO639a2: String, CaseIterable {
 
     // swiftlint:disable identifier_name
     case aa, ab, ae, af, ak, am, an, ar, `as`, av, ay, az,
-         ba, be, bg, bi, bm, bn, bo, bs,
+         ba, be, bg, bi, bm, bn, bo, br, bs,
          ca, ce, ch, co, cr, cs, cu, cv, cy,
          da, de, dv, dz,
          ee, el, en, eo, es, et, eu,
@@ -14,21 +14,22 @@ public enum ISO639a2: String, CaseIterable {
          ga, gd, gl, gn, gu, gv,
          ha, he, hi, ho, hr, hu, ht, hy, hz,
          ia, id, ie, ig, ii, ik, io, `is`, it, iu,
-         ja,
-         ka, kg, ki, kk, kj, km, kn, ko, kr, ks, ku, kv, kw, ky,
+         ja, jv,
+         ka, kg, ki, kj, kk, kl, km, kn, ko, kr, ks, ku, kv, kw, ky,
          la, lb, lg, li, ln, lo, lt, lu, lv,
          mg, mh, mi, mk, ml, mn, mr, ms, mt, my,
          na, nb, nd, ne, ng, nl, nn, no, np, nr, nv, ny,
-         oc, oj, om, or,
+         oc, oj, om, or, os,
          pa, pi, pl, ps, pt,
          qu,
          rm, rn, ro, ru, rw,
-         sc, sa, se, sd, sg, si, sk, sl, sm, sn, so, sq, sr, ss, st, su, sv, sw,
+         sa, sc, sd, se, sg, si, sk, sl, sm, sn, so, sq, sr, ss, st, su, sv, sw,
          ta, te, tg, th, ti, tl, tk, tn, to, tr, ts, tt, tw, ty,
          ug, uk, ur, uz,
          ve, vi, vo,
          wa, wo,
-         xh, yi, yo,
+         xh,
+         yi, yo,
          za, zh, zu
     // swiftlint:enable identifier_name
 }
