4cc3172ade9780df970af17c77daf7230bffb088
Philip Chu <technicat@users.noreply.github.com>
2023-04-05 18:03:26 -0700

    system language picker

--- Sources/SwiftSys/UI/Language/SystemLanguageMenu.swift
+++ Sources/SwiftSys/UI/Language/SystemLanguageMenu.swift
@@ -29,6 +29,26 @@ public struct SystemLanguageMenu: View {
     }
 }
 
+@available(macOS 13, *)
+@available(iOS 16, *)
+public struct SystemLanguagePicker: View {
+
+    @Binding var language: Locale.Language?
+
+    public init (_ language: Binding<Locale.Language?>) {
+        _language = language
+    }
+
+    public var body: some View {
+        Picker(selection: $language, label: Text("Language")) {
+            ForEach(Locale.Language.systemLanguages) { lang in
+                    Text(lang.display) 
+            }
+        }
+    }
+}
+
+
 
 
 
