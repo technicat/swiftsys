e368d24b380d8768486f765cedcb6d3ce0967b33
Philip Chu <technicat@users.noreply.github.com>
2023-04-05 18:18:30 -0700

    mastocode picker

--- Sources/SwiftSys/UI/Language/MastoCodeMenu.swift
+++ Sources/SwiftSys/UI/Language/MastoCodeMenu.swift
@@ -28,3 +28,20 @@ public struct MastoCodeMenu: View {
     }
 }
 
+public struct MastoCodePicker: View {
+    
+    @Binding var language: MastoCode
+
+    public init (_ language: Binding<MastoCode>) {
+        _language = language
+    }
+
+    public var body: some View {
+        Picker(selection: $language, label: Text("Language")) {
+            ForEach(MastoCode.allCases.sorted(by: { $0.display > $1.display })) { lang in
+                    Text(lang.display) 
+            }
+        }
+    }
+}
+
---
+++ Sources/SwiftSys/UI/Language/System/SystemLanguagePicker.swift
@@ -0,0 +1,30 @@
+//
+//  Created by Philip Chu on 4/4/23.
+//
+
+import SwiftUI
+
+/// minimum OS required for Language
+@available(macOS 13, *)
+@available(iOS 16, *)
+public struct SystemLanguagePicker: View {
+
+    @Binding var language: Locale.Language?
+
+    public init (_ language: Binding<Locale.Language?>) {
+        _language = language
+    }
+
+    public var body: some View {
+        Picker(selection: $language, label: Text("Language")) {
+            ForEach(Locale.Language.systemLanguages) { lang in
+                    Text(lang.display) 
+            }
+        }
+    }
+}
+
+
+
+
+
