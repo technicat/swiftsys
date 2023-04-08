024cbfd1e2ffcb2b9cf1b6b9b2c8eb61f7abc1d1
Philip Chu <technicat@users.noreply.github.com>
2023-04-08 13:03:02 -0700

    make Chinese Displayable

--- Sources/SwiftSys/Language/Chinese/Chinese.swift
+++ Sources/SwiftSys/Language/Chinese/Chinese.swift
@@ -1,6 +1,18 @@
 //  Created by Philip Chu on 7/31/15.
 //  Copyright (c) 2015 Technicat. All rights reserved.
+
 public enum Chinese: String, CaseIterable {
 
     case cantonese, mandarin, simplified
+}
+
+extension Chinese: Displayable {
+    public var display: String {
+        switch self {
+        case .cantonese: return "Cantonese"
+        case .mandarin: return "Mandarin"
+        case .simplified: return "Simplified"
+        }
+    }
+
 }
