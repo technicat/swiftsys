7e3f5ea6fa849651ffffe93d1471a33a390dcafc
Philip Chu <technicat@users.noreply.github.com>
2023-04-12 13:27:32 -0700

    move wiktionary processing to String

---
+++ Sources/SwiftSys/Extensions/String/StringWikt.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  
+//
+//  Created by Philip Chu on 4/12/23.
+//
+
+import Foundation
+import RegexBuilder
+
+@available(macOS 13, *)
+@available(iOS 16, *)
+extension String {
+    
+    private static let wiktRegex = Regex {
+        "{"
+        Capture(OneOrMore(.word))
+        "}"
+
+    }
+
+    public var expandedWikt: String {
+        let matches = Set(matches(of: String.wiktRegex).map { $0.output.1 })
+        var res = self
+        for match in matches {
+            if let site = Site(wkty: String(match)) {
+                res = res.replacingOccurrences(of: "{\(match)}",
+                                               with: site.markdown)
+            }
+        }
+        return res
+    }
+
+}
--- Sources/SwiftSys/Language/Item/ItemMarkdown.swift
+++ Sources/SwiftSys/Language/Item/ItemMarkdown.swift
@@ -2,38 +2,16 @@
 //  Copyright © 2017 Technicat. All rights reserved.
 
 import Foundation
-import RegexBuilder
 
 @available(macOS 13, *)
 @available(iOS 16, *)
 extension Item {
 
-    // make lazy?
-
     public var markdown: AttributedString {
-        (try? AttributedString(markdown: Item.processDesc(description),
+        (try? AttributedString(markdown: description.expandedWikt,
                                options: AttributedString.MarkdownParsingOptions(interpretedSyntax:
                                     .inlineOnlyPreservingWhitespace)))
                                     ?? AttributedString()
-    }
-
-    private static let regex = Regex {
-        "{"
-        Capture(OneOrMore(.word))
-        "}"
-
-    }
-
-    private static func processDesc(_ desc: String) -> String {
-        let matches = Set(desc.matches(of: regex).map { $0.output.1 })
-        var res = desc
-        for match in matches {
-            if let site = Site(wkty: String(match)) {
-                res = res.replacingOccurrences(of: "{\(match)}",
-                                               with: site.markdown)
-            }
-        }
-        return res
     }
 
 }
