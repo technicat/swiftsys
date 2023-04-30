315772bd600de0f6809adc0c620bb9e44e154e0e
Philip Chu <technicat@users.noreply.github.com>
2023-04-30 13:23:23 -0700

    Create BoolOnButton.swift

---
+++ Sources/SwiftSys/UI/Button/BoolOnButton.swift
@@ -0,0 +1,29 @@
+//
+//  Created by Philip Chu on 4/30/23.
+//
+
+import Foundation
+import SwiftUI
+
+/// Button that turns on a bool binding
+/// Typically used for activating sheets
+public struct BoolOnButton: View {
+    
+    let title: String
+    let image: String
+    
+    @Binding var sheet: Bool
+    
+    public init(_ title: String, _ image: String, _ sheet: Binding<Bool>) {
+        self.title = title
+        self.image = image
+        _sheet = sheet
+    }
+    
+    public var body:  some View {
+        ActionButton(title,
+                     sysImage: image) {
+            sheet = true
+        }
+    }
+}
