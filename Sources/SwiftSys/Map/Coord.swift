94783411020ef52c80e1eac221804a4eabae25a4
Philip Chu <technicat@users.noreply.github.com>
2023-07-24 18:12:21 -0600

    Update Coord.swift

--- Sources/SwiftSys/Map/Coord.swift
+++ Sources/SwiftSys/Map/Coord.swift
@@ -8,6 +8,11 @@ public struct Coord: Codable {
     public var lat: Double
     public var lon: Double
     
+    public init(lat: Double, lon: Double) {
+        self.lat = lat
+        self.lon = lon
+    }
+    
     public var mapCoord: CLLocationCoordinate2D {
         CLLocationCoordinate2D(latitude: lat, longitude: lon)
     }
