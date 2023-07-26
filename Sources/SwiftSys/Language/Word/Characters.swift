5c8f637799787a9c9d1d4dd47246639e0571d28a
Philip Chu <technicat@users.noreply.github.com>
2023-07-25 21:14:28 -0600

    Update Characters.swift

--- Sources/SwiftSys/Language/Word/Characters.swift
+++ Sources/SwiftSys/Language/Word/Characters.swift
@@ -2,12 +2,21 @@
 //  Copyright (c) 2015 Technicat. All rights reserved.
 
 /// characters accompanied by a list of explanatory (dictionary, wiktionary) links
-public struct Characters: Codable {
+public struct Characters {
     public var text: String
     public var refs: [Site]
-
+    
     public init(_ text: String, refs: [Site] = []) {
         self.text = text
         self.refs = refs
+    }
+}
+
+extension Characters: Codable {
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.text = try container.decode(String.self, forKey: .text)
+        self.refs = try container.decode([Site].self, forKey: .refs)
     }
 }
