bbd65554a1639f7ba447b22715ae1891f48013c1
Philip Chu <technicat@users.noreply.github.com>
2023-09-16 21:38:51 -0600

    Create CancelOffButton.swift

---
+++ Sources/SwiftSys/UI/Button/CancelOffButton.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  
+//
+//  Created by Philip Chu on 9/16/23.
+//
+
+import Foundation
+import SwiftUI
+
+@available(macOS 12.0, *)
+public struct CancelOffButton: View {
+
+    @Binding var doit: Bool
+    
+    public init(_ doit: Binding<Bool>) {
+        _doit = doit
+    }
+
+    public var body: some View {
+        Button("cancel", role: .cancel) {
+            doit = false
+        }
+    }
+}
+
