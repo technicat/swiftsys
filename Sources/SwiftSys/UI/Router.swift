2003af1f446ceec4177e1eab34d91fb64a5e5ea1
Philip Chu <technicat@users.noreply.github.com>
2023-10-01 13:50:20 -0600

    router

---
+++ Sources/SwiftSys/UI/Router.swift
@@ -0,0 +1,24 @@
+//
+//  Router.swift
+//  
+//
+//  Created by Philip Chu on 10/1/23.
+//
+
+import Foundation
+
+@available(macOS 10.15, *)
+public class Router<T>: ObservableObject {
+
+    @Published var path: [T] = []
+
+    func push(_ route: T) {
+        path.append(route)
+    }
+
+    func pop() {
+        if !path.isEmpty {
+            path.removeLast()
+        }
+    }
+}
