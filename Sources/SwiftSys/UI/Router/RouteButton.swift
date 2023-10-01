2e3a2b218b86839acc9198837bba04e13efe3539
Philip Chu <technicat@users.noreply.github.com>
2023-10-01 15:48:37 -0600

    routebutton

---
+++ Sources/SwiftSys/UI/Router/RouteButton.swift
@@ -0,0 +1,37 @@
+//
+//  RouteButton.swift
+//  
+//
+//  Created by Philip Chu on 10/1/23.
+//
+
+import Foundation
+import SwiftUI
+import TipKit
+
+@available(macOS 14.0, *)
+public struct RouteButton<T: Equatable>: View {
+
+    @EnvironmentObject var router: Router<T>
+
+    let title: String
+    let image: String
+    let route: T
+    let tip: (any Tip)?
+
+    public init(title: String, image: String, route: T, tip: (any Tip)? = nil) {
+        self.title = title
+        self.image = image
+        self.route = route
+        self.tip = tip
+    }
+
+    public var body: some View {
+        ActionButton(title, image) {
+            if let tip {
+                tip.invalidate(reason: .actionPerformed)
+            }
+            router.pushNew(route)
+        }
+    }
+}
