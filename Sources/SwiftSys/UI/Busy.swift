f900e3da344dad97a688dd59bb1c083ab6426292
Philip Chu <technicat@users.noreply.github.com>
2023-10-01 09:20:27 -0600

    Busy

---
+++ Sources/SwiftSys/UI/Busy.swift
@@ -0,0 +1,21 @@
+//
+//  Busy.swift
+//  
+//
+//  Created by Philip Chu on 10/1/23.
+//
+
+import SwiftUI
+
+@available(macOS 11.0, *)
+public struct Busy: View {
+
+    let ctr: Int
+
+    public var body: some View {
+        if ctr > 0 {
+            ProgressView()
+        }
+    }
+}
+
