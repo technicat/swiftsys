90597c80c1dc06bc0f65ce2ba0e65bde1a1c0310
Philip Chu <technicat@users.noreply.github.com>
2023-10-22 15:00:40 -0600

    Create UnimplementedView.swift

---
+++ Sources/SwiftSys/UI/UnimplementedView.swift
@@ -0,0 +1,18 @@
+//
+//  Created by Philip Chu on 10/22/23.
+//
+
+import Foundation
+import SwiftUI
+
+@available(macOS 14.0, *)
+public struct UnimplementedView: View {
+
+    public var body: some View {
+        ContentUnavailableView {
+            LLabel("unimplemented", "circle.slash")
+        } description: {
+            Text("unimplemented.description")
+        }
+    }
+}
