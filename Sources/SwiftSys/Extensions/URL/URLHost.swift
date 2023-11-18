79273ce52d722d2f3eaa8b672330c22b3a5834eb
Philip Chu <technicat@users.noreply.github.com>
2023-11-17 18:04:53 -0700

    Create URLHost.swift

---
+++ Sources/SwiftSys/Extensions/URL/URLHost.swift
@@ -0,0 +1,15 @@
+//
+//  Created by Philip Chu on 11/17/23.
+//
+
+import Foundation
+
+extension URL {
+    
+    var host: String? {
+        guard let comp = URLComponents(url: self, resolvingAgainstBaseURL: false) else {
+            return nil
+        }
+        return comp.host
+    }
+}
