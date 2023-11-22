df035ea919d0d47d16a2337495861963feb3cfbf
Philip Chu <technicat@users.noreply.github.com>
2023-11-22 13:33:16 -0700

    DeleteButton

--- Sources/SwiftSys/Protocols/Iconable.swift
+++ Sources/SwiftSys/Protocols/Iconable.swift
@@ -7,5 +7,6 @@ import Foundation
 public protocol Iconable {
 
     /// name of a system icon from SF Symbols
+    /// should rename, e.g. sfIcon, or SFIconable
     var icon: String { get }
 }
---
+++ Sources/SwiftSys/UI/Button/DeleteButton.swift
@@ -0,0 +1,20 @@
+import Foundation
+import SwiftUI
+
+@available(iOS 15.0, *)
+@available(macOS 12.0, *)
+public struct DeleteButton: View {
+
+    let action: () -> Void
+    
+    public init(action: @escaping () -> Void) {
+        self.action = action
+    }
+
+    var body: some View {
+        ActionButton("Delete", "trash",
+                     role: .destructive) {
+            action()
+        }
+    }
+}
