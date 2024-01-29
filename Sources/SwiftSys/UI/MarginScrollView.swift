ac75e8dc6dbb3ad2bea5513a202b08488ca2886c
Philip Chu <technicat@users.noreply.github.com>
2024-01-28 22:27:40 -0700

    Update MarginScrollView.swift

--- Sources/SwiftSys/UI/MarginScrollView.swift
+++ Sources/SwiftSys/UI/MarginScrollView.swift
@@ -1,15 +1,20 @@
 import SwiftUI
 
+@available(iOS 17.0, *)
 @available(macOS 14, *)
 public struct MarginScrollView<Content: View>: View {
 
   let margin = 10
 
-  @ViewBuilder let scroll: () -> Content
-
+ let content: Content
+    
+    public init(@ViewBuilder let content: @escaping () -> Content) {
+        self.content = content()
+    }
+    
   public var body: some View {
     ScrollView {
-      scroll()
+      content
     }
     .contentMargins(CGFloat(margin), for: .scrollContent)
   }
