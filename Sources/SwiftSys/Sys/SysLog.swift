f2936c50b814ceee4d9c52c9102c346926cc4e9e
Philip Chu <technicat@users.noreply.github.com>
2024-02-03 18:54:00 -0700

    Create SysLog.swift

---
+++ Sources/SwiftSys/Sys/SysLog.swift
@@ -0,0 +1,16 @@
+//
+//  SysLog.swift
+//  
+//
+//  Created by Philip Chu on 2/3/24.
+//
+
+//import OSLog
+//
+//extension Logger {
+//  /// Using your bundle identifier is a great way to ensure a unique identifier.
+//  static var subsystem = Bundle.main.bundleIdentifier!
+//
+//  /// Logs the view cycles like a view that appeared.
+//  static let log = Logger(subsystem: subsystem, category: "log")
+//}
